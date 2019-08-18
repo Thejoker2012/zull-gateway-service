a59114910afd4719aa0cc860d0179fa5d69d1d7d
Iago <iagofalcom@hotmail.com>
2019-08-18 17:39:58 -0300

    Initial commit

---
+++ src/main/java/br/com/iago/gatewayservice/zullgatewayservice/ZullGatewayServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.iago.gatewayservice.zullgatewayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZullGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZullGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/iago/gatewayservice/zullgatewayservice/ZullGatewayServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.iago.gatewayservice.zullgatewayservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZullGatewayServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
